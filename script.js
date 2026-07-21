71ba0f21bede5bc95af0303d73fa039d11a63c82
betonkri973-jpg <betonkri973@gmail.com>
2026-07-21 17:42:41 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+const loadingScreen = document.getElementById("loadingScreen");
+const menu = document.getElementById("menu");
+const game = document.getElementById("game");
+const startBtn = document.getElementById("startBtn");
+const bgMusic = document.getElementById("bgMusic");
+
+setTimeout(() => {
+    loadingScreen.style.display = "none";
+    menu.style.display = "block";
+}, 2000);
+
+startBtn.addEventListener("click", () => {
+
+    menu.style.display = "none";
+    game.style.display = "block";
+
+    bgMusic.volume = 0.4;
+    bgMusic.play().catch(err => {
+        console.log("Müzik başlatılamadı:", err);
+    });
+
+});
